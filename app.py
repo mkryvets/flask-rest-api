56602ff0fca1043b67f87dcab31e3bc34e87ec07
mkrivets <mkrivets1@gmail.com>
2020-06-04 00:09:41 +0300

    Added a Model of Hospital, created a Database

--- app.py
+++ app.py
@@ -5,6 +5,40 @@ import os
 
 # Init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Database initialization
+db = SQLAlchemy(app)
+
+# Marshmallow initialization
+ma = Marshmallow(app)
+
+# Hospital Model
+class Hospital(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), unique = True)
+    foundation_year = db.Column(db.Integer)
+    adress = db.Column(db.String(100), unique = True)
+    capacity = db.Column(db.Integer)
+
+    def __init__(self, name, foundation_year):
+        self.name = name
+        self.foundation_year = foundation_year
+        self.adress = adress
+        self.capacity = capacity
+
+# Hospital Schema
+class HospitalSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'foundation_year', 'adress', 'capacity')
+
+# Hospital schema initialization
+hospital_schema = HospitalSchema()
+hospitals_schema = HospitalSchema(many = True)
 
 # Run server
 if __name__ == '__main__':
