3147f34ede9b8fabbd4720759917267280e06ed0
mkrivets <mkrivets1@gmail.com>
2020-06-04 23:22:50 +0300

    Finished Hospital Model, implemented GET, POST, PUT, DELETE for it

--- app.py
+++ app.py
@@ -17,29 +17,93 @@ db = SQLAlchemy(app)
 # Marshmallow initialization
 ma = Marshmallow(app)
 
-# Hospital Model
+
+# Hospital Class/Model
 class Hospital(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
     foundation_year = db.Column(db.Integer)
-    adress = db.Column(db.String(100), unique = True)
+    adress = db.Column(db.String(100), unique=True)
     capacity = db.Column(db.Integer)
 
-    def __init__(self, name, foundation_year):
+    def __init__(self, name, foundation_year, adress, capacity):
         self.name = name
         self.foundation_year = foundation_year
         self.adress = adress
         self.capacity = capacity
+
 
 # Hospital Schema
 class HospitalSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'foundation_year', 'adress', 'capacity')
 
+
 # Hospital schema initialization
 hospital_schema = HospitalSchema()
-hospitals_schema = HospitalSchema(many = True)
+hospitals_schema = HospitalSchema(many=True)
+
+
+# Create a Hospital
+@app.route('/hospital', methods=['POST'])
+def app_hospital():
+    name = request.json['name']
+    foundation_year = request.json['foundation_year']
+    adress = request.json['adress']
+    capacity = request.json['capacity']
+
+    new_hospital = Hospital(name, foundation_year, adress, capacity)
+
+    db.session.add(new_hospital)
+    db.session.commit()
+
+    return hospital_schema.jsonify(new_hospital)
+
+
+# Get All Hospitals
+@app.route('/hospital', methods=['GET'])
+def get_hospitals():
+    all_hospitals = Hospital.query.all()
+    result = hospitals_schema.dump(all_hospitals)
+    return jsonify(result.data)
+
+
+# Get Single Hospital
+@app.route('/hospital/<id>', methods=['GET'])
+def get_hospital(id):
+    hospital = Hospital.query.get(id)
+    return hospital_schema.jsonify(hospital)
+
+
+# Update a Hospital
+@app.route('/hospital/<id>', methods=['PUT'])
+def update_hospital(id):
+    hospital = Hospital.query.get(id)
+
+    name = request.json['name']
+    foundation_year = request.json['foundation_year']
+    adress = request.json['adress']
+    capacity = request.json['capacity']
+
+    hospital.name = name
+    hospital.foundation_year = foundation_year
+    hospital.adress = adress
+    hospital.capacity = capacity
+
+    db.session.commit()
+
+    return hospital_schema.jsonify(hospital)
+
+
+# Delete Hospital
+@app.route('/hospital/<id>', methods=['DELETE'])
+def delete_hospital(id):
+    hospital = Hospital.query.get(id)
+    db.session.delete(hospital)
+    db.session.commit()
+    return hospital_schema.jsonify(hospital)
+
 
 # Run server
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', debug=True)
