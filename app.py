f2acfc126a8c2ef98124a9699b2d0ff7ce142c94
mkrivets <mkrivets1@gmail.com>
2020-06-05 06:27:16 +0300

    Created Department Model relative to Hospital, implemented GET, POST, PUT, DELETE for it

--- app.py
+++ app.py
@@ -21,10 +21,11 @@ ma = Marshmallow(app)
 # Hospital Class/Model
 class Hospital(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    foundation_year = db.Column(db.Integer)
-    adress = db.Column(db.String(100), unique=True)
-    capacity = db.Column(db.Integer)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    foundation_year = db.Column(db.Integer, nullable=False)
+    adress = db.Column(db.String(100), unique=True, nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    departments = db.relationship('Department', cascade='all,delete', backref='hospital')
 
     def __init__(self, name, foundation_year, adress, capacity):
         self.name = name
@@ -104,6 +105,82 @@ def delete_hospital(id):
     return hospital_schema.jsonify(hospital)
 
 
+# Department Class/Model
+class Department(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    hospital_id = db.Column(db.Integer, db.ForeignKey('hospital.id'))
+
+    def __init__(self, name, hospital_id):
+        self.name = name
+        self.hospital_id = hospital_id
+
+
+# Department Schema
+class DepartmentSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'hospital_id')
+
+
+# Department schema initialization
+department_schema = DepartmentSchema()
+departments_schema = DepartmentSchema(many=True)
+
+
+# Create a Department
+@app.route('/department', methods=['POST'])
+def app_department():
+    name = request.json['name']
+    hospital_id = request.json['hospital_id']
+
+    new_department = Department(name, hospital_id)
+
+    db.session.add(new_department)
+    db.session.commit()
+
+    return department_schema.jsonify(new_department)
+
+
+# Get All Departments
+@app.route('/department', methods=['GET'])
+def get_departments():
+    all_departments = Department.query.all()
+    result = departments_schema.dump(all_departments)
+    return jsonify(result.data)
+
+
+# Get Single Department
+@app.route('/department/<id>', methods=['GET'])
+def get_department(id):
+    department = Department.query.get(id)
+    return department_schema.jsonify(department)
+
+
+# Update a Department
+@app.route('/department/<id>', methods=['PUT'])
+def update_department(id):
+    department = Department.query.get(id)
+
+    name = request.json['name']
+    hospital_id = request.json['hospital_id']
+
+    department.name = name
+    department.hospital_id = hospital_id
+
+    db.session.commit()
+
+    return department_schema.jsonify(department)
+
+
+# Delete Department
+@app.route('/department/<id>', methods=['DELETE'])
+def delete_department(id):
+    department = Department.query.get(id)
+    db.session.delete(department)
+    db.session.commit()
+    return department_schema.jsonify(department)
+
+
 # Run server
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True)
+    app.run(debug=True)
